a0401b3175ac8ae8558387fb6966895107175075
Future <501864469@qq.com>
2022-11-22 18:34:02 +0800

    last

---
+++ ToStringExample.java
@@ -0,0 +1,14 @@
+public class ToStringExample{
+    public ToStringExample(int number){
+    }
+ 
+    public static void main(String[]args){
+        ToStringExample example=new ToStringExample(11);
+        /*example.setNumber(1234);
+        System.out.println(example.getNumber());*/
+        System.out.println(example.toString());
+        }
+}
+
+
+
